af83b995d82b698bef231ee0377e571afeb85a5e
steve <steve.quinlan@gmail.com>
2018-06-26 17:06:17 +0100

    Renaming postCandidates, Adding test

--- index.js
+++ index.js
@@ -2,27 +2,21 @@ const request = require('request');
 const crypto = require('crypto');
 const url = require('url');
 
-function ClinchTalent(key, secret, endpoint='https://api.clinchtalent.com/v1/') {
+function ClinchTalent(key, secret, endpoint = 'https://api.clinchtalent.com/v1/') {
   this.key = key;
   this.secret = secret;
-  this.endpoint=endpoint;
+  this.endpoint = endpoint;
 }
-
-// # GET
 
 ClinchTalent.prototype.getResource = function (resource, callback) {
   const preparedRequest = prepareRequest(this.key, this.secret, this.endpoint, resource);
   request.get(preparedRequest['url'], preparedRequest['options'], callback);
 };
 
-// # PATCH
-
 ClinchTalent.prototype.patchResource = function (resource, data, callback) {
   const preparedRequest = prepareRequest(this.key, this.secret, this.endpoint, resource, data);
   request.patch(preparedRequest['url'], preparedRequest['options'], callback);
 };
-
-// # POST
 
 ClinchTalent.prototype.postResource = function (resource, data, callback) {
   const preparedRequest = prepareRequest(this.key, this.secret, this.endpoint, resource, data);
@@ -30,7 +24,7 @@ ClinchTalent.prototype.postResource = function (resource, data, callback) {
 };
 
 
-// # Candidates
+// Candidates
 
 ClinchTalent.prototype.getCandidates = function (callback) {
   this.getResource('candidates', callback);
@@ -40,31 +34,31 @@ ClinchTalent.prototype.getCandidate = function (candidateId, callback) {
   this.getResource(`candidates/${candidateId}`, callback);
 };
 
-ClinchTalent.prototype.postCandidates = function (data, callback) {
+ClinchTalent.prototype.postCandidate = function (data, callback) {
   this.postResource('candidates', data, callback);
 };
 
-// # Themes
+// Themes
 
-ClinchTalent.prototype.getThemes = function(callback) {
-	this.getResource('themes', callback);
-}
+ClinchTalent.prototype.getThemes = function (callback) {
+  this.getResource('themes', callback);
+};
 
 ClinchTalent.prototype.getTheme = function (themeId, callback) {
   this.getResource(`themes/${themeId}`, callback);
 };
 
-ClinchTalent.prototype.getPageLayouts = function(themeId, callback) {
-    this.getResource(`themes/${themeId}/page_layouts`, callback);
-}
+ClinchTalent.prototype.getPageLayouts = function (themeId, callback) {
+  this.getResource(`themes/${themeId}/page_layouts`, callback);
+};
 
-ClinchTalent.prototype.getPageLayout = function(themeId, pageLayoutId, callback) {
-    this.getResource(`themes/${themeId}/page_layouts/${pageLayoutId}`, callback);
-}
+ClinchTalent.prototype.getPageLayout = function (themeId, pageLayoutId, callback) {
+  this.getResource(`themes/${themeId}/page_layouts/${pageLayoutId}`, callback);
+};
 
-ClinchTalent.prototype.patchPageLayout = function(themeId, pageLayoutId, data, callback) {
-    this.patchResource(`themes/${themeId}/page_layouts/${pageLayoutId}`, data, callback);
-}
+ClinchTalent.prototype.patchPageLayout = function (themeId, pageLayoutId, data, callback) {
+  this.patchResource(`themes/${themeId}/page_layouts/${pageLayoutId}`, data, callback);
+};
 
 // # HMAC
 
@@ -74,27 +68,27 @@ function generateHmac(data, secretKey) {
 
 // # Prepare HTTP Requests
 
-function prepareRequest(key, secret, endpoint, resource, data=null) {
-	const urlComponents = url.parse(endpoint);
-    const date = new Date().toUTCString();
-	const path = urlComponents.pathname + resource
-    const contentType = data !== null ? 'application/json' : '';
-    const contentMD5 = ''
-    const canonicalString = [contentType, contentMD5, path, date].join(',');
+function prepareRequest(key, secret, endpoint, resource, data = null) {
+  const urlComponents = url.parse(endpoint);
+  const date = new Date().toUTCString();
+  const path = urlComponents.pathname + resource;
+  const contentType = data !== null ? 'application/json' : '';
+  const contentMD5 = '';
+  const canonicalString = [contentType, contentMD5, path, date].join(',');
 
-    var options = {
-      'headers': {
-        'Date': date,
-        'Accept': 'application/vnd.api+json',
-        'Authorization': 'APIAuth ' + key + ":" + generateHmac(canonicalString, secret)
-      },
-    };
-	
-	if (data) {
-		options = Object.assign(options, {'body': data, 'json': true})
-	}
-	
-	return {url: `${urlComponents.protocol}//${urlComponents.host}${path}`, options: options};
+  let options = {
+    'headers': {
+      'Date': date,
+      'Accept': 'application/vnd.api+json',
+      'Authorization': 'APIAuth ' + key + ":" + generateHmac(canonicalString, secret)
+    },
+  };
+
+  if (data) {
+    options = Object.assign(options, {'body': data, 'json': true})
+  }
+
+  return {url: `${urlComponents.protocol}//${urlComponents.host}${path}`, options: options};
 }
 
 module.exports = ClinchTalent;
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-var expect = require('chai').expect;
-var request = require('request');
-var ClinchTalent = require('./index.js');
-var sinon = require('sinon');
+const expect = require('chai').expect;
+const request = require('request');
+const ClinchTalent = require('./index.js');
+const sinon = require('sinon');
 
 describe('ClinchTalent', function () {
   it('should initialize with a key and secret', function () {
-    var client = new ClinchTalent('YOUR-KEY', 'YOUR-SECRET');
+    const client = new ClinchTalent('YOUR-KEY', 'YOUR-SECRET');
 
     expect(client).to.have.a.property('key', 'YOUR-KEY');
     expect(client).to.have.a.property('secret', 'YOUR-SECRET');
@@ -23,8 +23,8 @@ describe('ClinchTalent', function () {
     });
 
     it('#getResource()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:8SIVQDCMltzKc16AvB3/UivbqNU=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:8SIVQDCMltzKc16AvB3/UivbqNU=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://api.clinchtalent.com/v1/resource', expectedHeaders).once();
       this.client.getResource('resource', function () {
       });
@@ -32,53 +32,62 @@ describe('ClinchTalent', function () {
     });
 
     it('#getCandidates()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:cgLyS9HCvKFQMvoidj3sWphhYLk=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:cgLyS9HCvKFQMvoidj3sWphhYLk=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://api.clinchtalent.com/v1/candidates', expectedHeaders).once();
       this.client.getCandidates(function () {
       });
       mock.verify();
     });
-	
+
+    it('#postCandidate()', function () {
+      const mock = sinon.mock(request);
+      //couldn't match on parameters..
+      mock.expects('post').withArgs('https://api.clinchtalent.com/v1/candidates', sinon.match.any, sinon.match.any).once();
+      this.client.postCandidate({data: {attributes: {first_name: "FN", last_name: "SN"}}}, function () {
+      });
+      mock.verify();
+    });
+
     it('#getThemes()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:4nZeylg4guRy7qhE1BDMSoF7vt8=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:4nZeylg4guRy7qhE1BDMSoF7vt8=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://api.clinchtalent.com/v1/themes', expectedHeaders).once();
       this.client.getThemes(function () {
       });
       mock.verify();
     });
-	
+
     it('#getTheme()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:xswV13clrzrVKzEgZufsF25TsiY=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:xswV13clrzrVKzEgZufsF25TsiY=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://api.clinchtalent.com/v1/themes/abc123', expectedHeaders).once();
       this.client.getTheme('abc123', function () {
       });
       mock.verify();
     });
-	
+
     it('#getPageLayouts()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:1E0D1A88RMrlJQjc9OrCvzWcZX0=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:1E0D1A88RMrlJQjc9OrCvzWcZX0=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://api.clinchtalent.com/v1/themes/abc123/page_layouts', expectedHeaders).once();
       this.client.getPageLayouts('abc123', function () {
       });
       mock.verify();
     });
-	
+
     it('#getPageLayout()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:RRVWkOsSzoOHPwiR9BHmmoVNQEE=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:RRVWkOsSzoOHPwiR9BHmmoVNQEE=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://api.clinchtalent.com/v1/themes/abc123/page_layouts/def123', expectedHeaders).once();
       this.client.getPageLayout('abc123', 'def123', function () {
       });
       mock.verify();
     });
-	
+
     it('#patchPageLayout()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:OBGOKGl+AcMMzlI1r5QixDg+Ypo=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}, body: {html: '<html></html>'}, json: true};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:OBGOKGl+AcMMzlI1r5QixDg+Ypo=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}, body: {html: '<html></html>'}, json: true};
       mock.expects('patch').withArgs('https://api.clinchtalent.com/v1/themes/abc123/page_layouts/def123', expectedHeaders).once();
       this.client.patchPageLayout('abc123', 'def123', {html: '<html></html>'}, function () {
       });
@@ -86,33 +95,33 @@ describe('ClinchTalent', function () {
     });
 
     it('#getResource()', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:O3GXlD8wSyl7rn4LLKk5Ve2h/xQ=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:O3GXlD8wSyl7rn4LLKk5Ve2h/xQ=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://api.clinchtalent.com/v1/candidates/abc123', expectedHeaders).once();
       this.client.getCandidate('abc123', function () {
       });
       mock.verify();
     });
-	
+
     it('#patchResource()', function () {
-	  var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:OBGOKGl+AcMMzlI1r5QixDg+Ypo=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}, body: {html: '<html></html>'}, json: true};
+	  const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:OBGOKGl+AcMMzlI1r5QixDg+Ypo=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}, body: {html: '<html></html>'}, json: true};
       mock.expects('patch').withArgs('https://api.clinchtalent.com/v1/themes/abc123/page_layouts/def123', expectedHeaders).once();
       this.client.patchPageLayout('abc123', 'def123', {html: '<html></html>'}, function () {
       });
       mock.verify();
     });
-	
+
     it('#alternativeEndpoint', function () {
-      var mock = sinon.mock(request);
-      var expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:O3GXlD8wSyl7rn4LLKk5Ve2h/xQ=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
+      const mock = sinon.mock(request);
+      const expectedHeaders = {headers: {Accept: 'application/vnd.api+json', Authorization: 'APIAuth YOUR-KEY:O3GXlD8wSyl7rn4LLKk5Ve2h/xQ=', Date: 'Thu, 01 Jan 1970 00:00:00 GMT'}};
       mock.expects('get').withArgs('https://alternative.endpoint.local/v1/candidates/abc123', expectedHeaders).once();
       this.client = new ClinchTalent('YOUR-KEY', 'YOUR-SECRET', 'https://alternative.endpoint.local/v1/');
       this.client.getCandidate('abc123', function () {
       });
       mock.verify();
     });
-	
-	
+
+
   });
 });
